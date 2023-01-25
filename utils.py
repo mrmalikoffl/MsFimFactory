4e635d607bbf543279be3e155e38bf08c7c5c310
mrmalikoffl <121479735+mrmalikoffl@users.noreply.github.com>
2023-01-25 11:32:51 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -375,30 +375,3 @@ def humanbytes(size):
         size /= power
         n += 1
     return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
-
-async def get_shortlink(link):
-    https = link.split(":")[0]
-    if "http" == https:
-        https = "https"
-        link = link.replace("http", https)
-
-    url = f'https://tnlink.in/api'
-    params = {'token': URL_SHORTNER_WEBSITE_API,
-              'link': link,
-              'format': 'json'
-              }
-
-    try:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
-                data = await response.json(content_type='text/html')
-                if data["status"] == "success":
-                    return data['shortlink']
-                else:
-                    logger.error(f"Error: {data['message']}")
-                    return f'https://tnlink.in/api?token={URL_SHORTNER_WEBSITE_API}&link={link}'
-
-    except Exception as e:
-        logger.error(e)
-        return f'https://tnlink.in/api?token={URL_SHORTNER_WEBSITE_API}&link={link}'
-
